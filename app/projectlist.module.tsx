871608c7cc061c5ecd5dcfc1062dc7d286f344d0
aelithron <aelithron@gmail.com>
2025-05-10 10:16:47 -0600

    Adjust a little css

--- app/projectlist.module.tsx
+++ app/projectlist.module.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 
 export default function ProjectList() {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mt-2">
+    <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
       <div className="flex flex-col border-slate-700 border-2 mt-5 p-3 rounded-lg">
         <Image src={logo} alt={"The project's icon."} width={75} height={75} className="border-slate-600 border-2 rounded-2xl" />
         <h1 className="text-xl font-semibold">Project 1</h1>
